a1ae808f0729b58d7031d25480145a4a3b1fae5a
Dmitriy Sumaroka <sumaroka.dmitriy@gmail.com>
2024-09-09 18:41:23 +0300

    Created BookServiceImpl and implemented methods. Created BookRepositoryImpl

---
+++ src/main/java/mate/academy/springbootintro/repository/impl/BookRepositoryImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.springbootintro.repository.impl;
+
+import java.util.List;
+import mate.academy.springbootintro.model.Book;
+import mate.academy.springbootintro.repository.BookRepository;
+
+public class BookRepositoryImpl implements BookRepository {
+    @Override
+    public Book save(Book book) {
+        return null;
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return null;
+    }
+}
---
+++ src/main/java/mate/academy/springbootintro/service/impl/BookServiceImpl.java
@@ -0,0 +1,21 @@
+package mate.academy.springbootintro.service.impl;
+
+import java.util.List;
+import mate.academy.springbootintro.model.Book;
+import mate.academy.springbootintro.repository.BookRepository;
+import mate.academy.springbootintro.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookRepository bookRepository;
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public List<Book> findAll() {
+        return bookRepository.findAll();
+    }
+}
