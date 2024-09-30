697ada74327c5fbcbd2a8c62315d241ad3e62163
dsumaroka <132662256+dsumaroka@users.noreply.github.com>
2024-09-30 18:01:21 +0300

    Update BookRepositoryImpl.java

--- src/main/java/springboot/web/example/repository/impl/BookRepositoryImpl.java
+++ src/main/java/springboot/web/example/repository/impl/BookRepositoryImpl.java
@@ -1,1 +1,55 @@
+package springboot.web.example.service.impl;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import springboot.web.example.dto.BookDto;
+import springboot.web.example.dto.CreateBookRequestDto;
+import springboot.web.example.exception.EntityNotFoundException;
+import springboot.web.example.mapper.BookMapper;
+import springboot.web.example.model.Book;
+import springboot.web.example.repository.BookRepository;
+import springboot.web.example.service.BookService;
+
+@Service
+@RequiredArgsConstructor
+public class BookServiceImpl implements BookService {
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
+
+    @Override
+    public BookDto save(CreateBookRequestDto createBookRequestDto) {
+        Book book = bookMapper.toBookEntity(createBookRequestDto);
+        bookRepository.save(book);
+        return bookMapper.toBookDto(book);
+    }
+
+    @Override
+    public List<BookDto> findAll() {
+        return bookRepository.findAll().stream()
+                .map(bookMapper::toBookDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find in DB book with id: " + id));
+        return bookMapper.toBookDto(book);
+    }
+
+    @Override
+    public BookDto updateBookById(Long id, CreateBookRequestDto createBookRequestDto) {
+        Book book = bookRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Can't find in DB book with id: " + id));
+        bookMapper.updateBookFromDto(createBookRequestDto, book);
+        bookRepository.save(book);
+        return bookMapper.toBookDto(book);
+    }
+
+    @Override
+    public void deletedById(Long id) {
+        bookRepository.deleteById(id);
+    }
+}
 
