47981afbafdb3ce57af4712b206cf8efa9259319
dsumaroka <sumaroka.dmitriy@gmail.com>
2024-10-08 17:27:13 +0300

    commit before merging

--- src/main/java/springboot/web/example/model/Book.java
+++ src/main/java/springboot/web/example/model/Book.java
@@ -31,8 +31,7 @@ public class Book {
     @Column(nullable = false)
     private BigDecimal price;
     private String description;
-    @Column(name = "cover_image")
     private String coverImage;
-    @Column(name = "is_deleted", nullable = false, columnDefinition = "TINYINT(1)")
+    @Column(nullable = false, columnDefinition = "TINYINT(1)")
     private boolean isDeleted = false;
 }
