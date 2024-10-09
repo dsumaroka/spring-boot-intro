d41585d3317d3a52bfe583c664e8336b017edbe4
dsumaroka <132662256+dsumaroka@users.noreply.github.com>
2024-10-09 17:56:46 +0300

    Update BookRepository.java

--- src/main/java/springboot/web/example/repository/BookRepository.java
+++ src/main/java/springboot/web/example/repository/BookRepository.java
@@ -1,7 +1,1 @@
-package springboot.web.example.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import springboot.web.example.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
-}
